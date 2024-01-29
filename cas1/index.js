d030fb1000066d8e9282e29982bb74900601c250
Suad Suljovic <suad.suljovic92@gmail.com>
2024-01-29 21:16:05 +0100

    first class finished

--- cas1/index.js
+++ cas1/index.js
@@ -21,3 +21,81 @@
 // closure
 // classes
 // es6 js
+
+// var newVar = "ssadf";
+
+// const a = 20;
+
+// let b = 30;
+
+// function name(params) {
+//   var c = "ss";
+// }
+
+// console.log(1 + "2" + "false" + false); // 12falsefalse
+// console.log(1 + 2 + true + false); //  4
+// console.log(1 + 2 + "_" + false); // 3_false
+// console.log("1" + "2" + (12 + 12)); // 1224
+
+// truthy vs falsy
+// falsy: 0, "", NaN, undefined, null, false,
+
+// if (!!!!!!document) {
+//   console.log("radi");
+// }
+
+// console.log("a" && "b" && undefined); // undefined
+// console.log("a" && 0 && "yes"); // 0
+// console.log("a" && "b" && "mozda"); // mozda
+
+// console.log("" || false || "true"); // "true"
+// console.log("" || 0 || false); // false
+// console.log("yes" || 0 || false); // "yes"
+
+// if (0) {
+//   console.log("yeas");
+// } else if (0) {
+//   console.log("no");
+// } else if (0) {
+// }
+
+// let check = true;
+
+// if (check) {
+//   console.log("uslov je tacan");
+// } else {
+//   console.log("uslov nije tacan");
+// }
+
+// 95> 10, 85 > 95,
+
+// const ocena = 83
+
+// if(ocena > 95){
+//     console.log(10)
+// }else if(ocena> 85){
+//     console.log(9)
+// }else if(ocena> 75){
+//     console.log(9)
+// }
+
+// const ocena = 3;
+
+// switch (ocena) {
+//   case 1:
+//     console.log("jedan");
+//     break;
+//   case 2:
+//     console.log("dva");
+//     break;
+//   case 3:
+//     console.log("tri");
+//     break;
+//   case 4:
+//     console.log("cetri");
+//     break;
+// }
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
