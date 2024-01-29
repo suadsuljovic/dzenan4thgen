bd19e1a015b36b5e5a0a12c1d36159f13f7140a1
Suad Suljovic <suad.suljovic92@gmail.com>
2024-01-29 20:14:24 +0100

    first commit

---
+++ cas1/index.js
@@ -0,0 +1,23 @@
+// primitives
+// complex types
+// const let
+// operators
+// logical operators
+// conditionals
+// loops ✅
+// objects
+// arrays
+// array methods
+// callbacks
+
+// promises
+// event loop
+// async await
+// DOM & BOM
+// DOM manipulation
+// events
+
+// hoisting
+// closure
+// classes
+// es6 js
