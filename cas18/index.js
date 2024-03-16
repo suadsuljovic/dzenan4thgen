e9a2c489956b6bc77ec4961085937618a2275554
Suad Suljovic <suad.suljovic92@gmail.com>
2024-03-16 20:49:42 +0100

    cas18

---
+++ cas18/index.js
@@ -0,0 +1,44 @@
+// test prep
+
+// Test has 2 parts
+
+// 1st part
+// Imacete 20 minuta za ovaj deo. I radicete na google formi.
+// Necete imati pravo na korsticenje interneta.
+// Imate 10tak teroriskih pitanja iz oblasti navdene u 17-tom casu
+// najvise pitanja iz simple js i 2-3 pitanja iz advanced i medium js
+
+// select a primitive js type? i onda abcd, odgovori
+// what are complext types in js? abcd
+// what are the scopes of const, let, and var
+// what are the differces between let and const...
+// what is the result of the code console.log("yes" || "no")?
+// what is the result of the code if(true){console.log(true)}
+// what are the loops use in js?
+// what is an object in js?
+// how do we dinamically access values inside of the object?
+// What are the property accessor for objects?
+// Which are the array fuctions?
+// What does this code do? [].map(()=>{})
+// advanced only definitions of concepts.
+
+// Part 2
+// Practical part.
+// Imacete 40 minuta za ovaj deo. Po potrebi i vise.
+// Imacete pristup internetu i svemu sto smo radili do sada.
+// chat gpt ne dolazi u obzir.
+// Imacete 2 pitanja
+// Morate napraviti repostori na svom github nalogu gde ce te pushati kod
+// link ka repozitoriju ce te ostaviti na google formu za ovaj deo
+
+// Prvo pitanje:
+// Bice vise algoritamskog tipa gde imate neku obradu podataka i vi
+// trebate nesto sa tim podacima da uradite. 5,6,7 cas
+
+// Drugo pitanje:
+// Trebate fetchati nesto sa nekog apija i onda to prikazati na stranici
+// Moguca dodatna komplikacija gde ce te morati i na neki event da fetchate podatke
+
+// imate primere od 9 casa pa nadalje. A navise na casu 15.
+
+// Q&A
